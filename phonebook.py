a02dbf46faffb1e55155a3e8f4be5ff472f9d538
oleksandrzinoviev <shurik.zinovyev@gmail.com>
2017-04-04 20:04:54 +0300

    json read and write

--- phonebook.py
+++ phonebook.py
@@ -1,4 +1,4 @@
-contacts = {}
+import json, csv
 
 def input_values(param):
     return input(param)
@@ -28,9 +28,10 @@ def create_contact():
     if phone_exist == False:
         phone = input_phone()
         contacts[name] = phone
+        save_into_file(name)
         print("Contact", name, "with phone:", phone, "created in phone book")
     elif phone_exist == True:
-        print("Contact with the same name can't be crated")
+        print("Contact with the same name can't be created")
 
 
 def read_contact():
@@ -44,6 +45,7 @@ def update_contact():
     if phone_exist == True:
         phone = input_phone()
         contacts[name] = phone
+        save_into_file(name)
         print("Contact", name, "has been updated with phone:", phone)
 
 
@@ -51,6 +53,7 @@ def delete_contact():
     name, phone_exist = check_contact()
     if phone_exist == True:
         contacts.pop(name)
+        save_into_file(name)
         print("Contact with name ", name, " has been removed")
 
 
@@ -70,8 +73,34 @@ def choose_operation():
             update_contact()
         elif operation == 'D':
             delete_contact()
+        elif operation == 'Q':
+            quit()
         else:
             print(operation, "is unsupported operation. Please choose operation from C R U D")
             continue
 
+
+def save_into_file(name):
+    with open('f.json', 'wt') as json_file:
+        json.dump(contacts, json_file)
+    with open('f.csv', 'wt') as csv_file:
+        w = csv.writer(csv_file)
+        w.writerow({contacts[name],name})
+
+
+def read_from_file():
+    try:
+        with open('f.json', 'rt') as file:
+            return json.load(file)
+    except FileNotFoundError:
+        print("Oops, file is absent")
+        return {}
+
+
+
+contacts = read_from_file()
 choose_operation()
+
+
+w = csv.writer(f)
+#     w.writerow(('aa aa', 1234))

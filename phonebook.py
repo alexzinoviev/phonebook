87f6388ccafa6241245288b8a61fa7d57432a98c
oleksandrzinoviev <shurik.zinovyev@gmail.com>
2017-05-01 15:12:20 +0300

    refactored, using of class instances

--- phonebook.py
+++ phonebook.py
@@ -2,7 +2,6 @@ import json
 import csv
 import configparser
 
-contacts = {}
 
 class Controller:
     def read_config(self):
@@ -12,34 +11,34 @@ class Controller:
         return file_type
 
     def read_from_file(self):
-        file_type = self.read_config(self)
+        file_type = self.read_config()
         if file_type == 'json':
-            return JSONFile.read_from_file(JSONFile)
+            return json_file.read_from_file()
         elif file_type == 'csv':
-            return CSVFile.read_from_file(CSVFile)
+            return csv_file.read_from_file()
 
     def save_into_file(self, name):
-        file_type = self.read_config(self)
+        file_type = self.read_config()
         if file_type == 'json':
-            JSONFile.save_into_file(JSONFile, name)
+            json_file.save_into_file(name)
         elif file_type == 'csv':
-            CSVFile.save_into_file(CSVFile, name)
+            csv_file.save_into_file(name)
 
     def choose_operation(self):
         execute = False
-        OPERATIONS = {'C': Operations.create_contact, 'R': Operations.display_results,
-                      'U': Operations.update_contact, 'D': Operations.delete_contact, 'Q': quit}
+        possible_operations = {'C': operations.create_contact, 'R': operations.display_results,
+                               'U': operations.update_contact, 'D': operations.delete_contact, 'Q': quit}
         while True:
-            operation = Operations.input_values(self, "Enter operation type: ")
-            for oper in OPERATIONS:
-                if operation == oper:
-                    execute = OPERATIONS[oper]
-                    execute(self)
+            chosen_operation = operations.input_values("Enter operation type: ")
+            for operation in possible_operations:
+                if chosen_operation == operation:
+                    execute = possible_operations[operation]
+                    execute()
             if execute == False:
-                print(operation, "is unsupported operation. Please choose operation from C R U D")
+                print(chosen_operation, "is unsupported operation. Please choose operation from C R U D")
 
 
-class CSVFile(Controller):
+class CSVFile:
     def save_into_file(self, name):
         with open('f.csv', 'w') as csv_file:
             writer = csv.writer(csv_file)
@@ -56,7 +55,7 @@ class CSVFile(Controller):
             return {}
 
 
-class JSONFile(Controller):
+class JSONFile:
     def save_into_file(self, name):
         with open('f.json', 'wt') as json_file:
             json.dump(contacts, json_file)
@@ -75,13 +74,13 @@ class Operations:
         return input(param)
 
     def input_name(self):
-        return Operations.input_values(self, "Please enter Name: ")
+        return operations.input_values("Please enter Name: ")
 
     def input_phone(self):
-        return Operations.input_values(self, "Please enter Phone: ")
+        return operations.input_values("Please enter Phone: ")
 
     def check_contact(self):
-        name = Operations.input_name(self)
+        name = operations.input_name()
         if name in contacts:
             phone_exist = True
             return name, phone_exist
@@ -91,39 +90,45 @@ class Operations:
             return name, phone_exist
 
     def create_contact(self):
-        name, phone_exist = Operations.check_contact(self)
+        name, phone_exist = operations.check_contact()
         if phone_exist == False:
-            phone = Operations.input_phone(self)
+            phone = operations.input_phone()
             contacts[name] = phone
-            Controller.save_into_file(self, name)
+            controller.save_into_file(name)
             print("Contact", name, "with phone:", phone, "created in phone book")
         elif phone_exist == True:
             print("Contact with the same name can't be created")
 
     def read_contact(self):
-        name, phone_exist = Operations.check_contact(self)
+        name, phone_exist = operations.check_contact()
         if phone_exist == True:
             return name, contacts[name]
 
     def update_contact(self):
-        name, phone_exist = Operations.check_contact(self)
+        name, phone_exist = operations.check_contact()
         if phone_exist == True:
-            phone = Operations.input_phone(self)
+            phone = operations.input_phone()
             contacts[name] = phone
-            Controller.save_into_file(self, name)
+            controller.save_into_file(name)
             print("Contact", name, "has been updated with phone:", phone)
 
     def delete_contact(self):
-        name, phone_exist = Operations.check_contact(self)
+        name, phone_exist = operations.check_contact()
         if phone_exist == True:
             contacts.pop(name)
-            Controller.save_into_file(self, name)
+            controller.save_into_file(name)
             print("Contact with name ", name, " has been removed")
 
     def display_results(self):
-        print(Operations.read_contact(self))
+        print(operations.read_contact())
 
 
 if __name__ == '__main__':
-    contacts = Controller.read_from_file(Controller)
-    Controller.choose_operation(Controller)
+    json_file = JSONFile()
+    csv_file = CSVFile()
+    controller = Controller()
+    operations = Operations()
+
+    contacts = controller.read_from_file()
+    print(contacts)
+    controller.choose_operation()
